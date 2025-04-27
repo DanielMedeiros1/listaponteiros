5ff3fa2c1563ff6eb9296f40e9c42652e102f216
DanielMedeiros1 <112391702+DanielMedeiros1@users.noreply.github.com>
2025-04-27 14:46:52 -0300

    Delete listaponteiros directory

--- listaponteiros/questao14.c
+++
@@ -1,7 +0,0 @@
-// Dentro do main(), é dito que c = 5, b = &c e a = &b, após isso a função f é chamada e seus parâmetros são passado como sendo f(c,b,a).
-// Isso nos mostra que c esta sendo passado como valor de a, logo dentro da função é feito uma conta que é a += 3, logo a = 5+3 = 8.
-// O proximo passo é achar o valor de c dentro da função, esse que foi declarado no main() como sendo igual a 5. Logo quando entramos na 
-// função, percebemos uma conta que esta incrementando esse valor de c em mais uma unidade, ou seja **ppc += 1, logo c = 5+1 = 6
-// O penúltimo passo é achar o valor de b, que é declarado no main() como sendo igual ao endereço de c, logo o ponteiro pega o conteúdo de c, 
-// que dentro da função é igual a 6, e realiza uma nova conta que seria o incremento de duas unidades ao *pb, sendo assim, b+=2, b=6+2 = 8.
-// Por fim, é realizado um dentro da função um return a + b + c (8+8+6) que resulta no valor 22 e que é mostrado como resposta do programa. 
--- listaponteiros/questao25.c
+++
@@ -1,3 +0,0 @@
-// A alternativa C é a que contém vazamento de memória, devido ao uso da função strlen, 
-// que acaba procurando mais de 50 caracteres e com isso não permite que a memória seja 
-// liberada pela função free() 
